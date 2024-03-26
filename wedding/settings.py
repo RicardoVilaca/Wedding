6ce29e31499623f6b8441751e1b8720a5436b77e
Ricardo Vilaça <ricardossv18@hotmail.com>
2024-03-26 15:03:31 +0000

    Update settings.py

--- wedding/settings.py
+++ wedding/settings.py
@@ -131,3 +131,5 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
