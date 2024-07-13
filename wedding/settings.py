9a460324a985cb9ed41eed553d6f4f20950b10f9
Ricardo Vilaça <ricardossv18@hotmail.com>
2024-07-13 21:30:26 +0100

    Updated allowed hosts

--- wedding/settings.py
+++ wedding/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "yOwbLWyXow3kJjwS2M6NTaz1qNAwVjhbnDoScS3REgABEXAwiv"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://weddingst-c6ac5e2f00b5.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://weddingst-c6ac5e2f00b5.herokuapp.com/', '127.0.0.1', '*sofia-tomas.com']
 
 
 # Application definition
