8e66b2b58efa23e4ed2e9b1e583445432eb9d8ae
Ricardo Vilaça <ricardossv18@hotmail.com>
2024-07-15 12:52:02 +0100

    Update settings.py

--- wedding/settings.py
+++ wedding/settings.py
@@ -30,8 +30,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['https://weddingst-c6ac5e2f00b5.herokuapp.com/', '127.0.0.1', '*sofia-tomas.com']
 CSRF_TRUSTED_ORIGINS = [
-    'https://weddingst-c6ac5e2f00b5.herokuapp.com/',
-    'https://sofia-tomas.com/'
+    'https://weddingst-c6ac5e2f00b5.herokuapp.com',
+    'https://sofia-tomas.com'
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
